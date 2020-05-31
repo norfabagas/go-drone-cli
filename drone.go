af430342db876ec60a11863d709a346ee5319aab
Norfa Bagas <bgsnorfa@gmail.com>
2020-05-31 23:39:40 +0700

    Update error message on drone method to use error object

--- drone.go
+++ drone.go
@@ -1,11 +1,13 @@
 package main
 
+import "errors"
+
 /*
 Coordinate tuple (x,y)
- */
+*/
 type Coordinate struct {
-	X	uint64
-	Y	uint64
+	X uint64
+	Y uint64
 }
 
 /*
@@ -13,7 +15,7 @@ Drone struct with
 MinPoint and MaxPoint defines drone borderline in (x,y)
 CurrentPoint defines drone current position in (x,y)
 Face defines drone direction
- */
+*/
 type Drone struct {
 	MinPoint     Coordinate
 	MaxPoint     Coordinate
@@ -25,7 +27,7 @@ type Drone struct {
 Get current drone position
 based on CurrentPoint and Face value
 return current(Coordinate), min(Coordinate), max(Coordinate), string
- */
+*/
 func (drone *Drone) GetCurrentPosition() (Coordinate, Coordinate, Coordinate, string) {
 	return drone.CurrentPoint, drone.MinPoint, drone.MaxPoint, DroneFace()[drone.Face]
 }
@@ -34,7 +36,7 @@ func (drone *Drone) GetCurrentPosition() (Coordinate, Coordinate, Coordinate, st
 Change drone face direction
 Input: integer 1-4
 Output: bool
- */
+*/
 func (drone *Drone) SetDroneFace(direction uint) bool {
 	droneFace := DroneFace()
 	if _, status := droneFace[direction]; status {
@@ -49,46 +51,46 @@ func (drone *Drone) SetDroneFace(direction uint) bool {
 Move drone from current position
 Input: steps(uint64)
 Output: bool, string
- */
-func (drone *Drone) SetDronePosition(steps uint64) (bool, string) {
+*/
+func (drone *Drone) SetDronePosition(steps uint64) (bool, error) {
 	switch drone.Face {
 	case 1:
 		if result := drone.CurrentPoint.Y + steps; result <= drone.MaxPoint.Y {
 			drone.CurrentPoint.Y = result
-			return true, ""
+			return true, nil
 		} else {
-			return false, "Drone cannot move outside of Max Y Point"
+			return false, errors.New("Drone cannot move outside of Max Y Point")
 		}
 	case 2:
 		if steps <= drone.CurrentPoint.Y {
 			drone.CurrentPoint.Y = drone.CurrentPoint.Y - steps
-			return true, ""
+			return true, nil
 		} else {
-			return false, "Drone cannot move outside of Min Y Point"
+			return false, errors.New("Drone cannot move outside of Min Y Point")
 		}
 	case 3:
 		if steps <= drone.CurrentPoint.X {
 			drone.CurrentPoint.X = drone.CurrentPoint.X - steps
-			return true, ""
+			return true, nil
 		} else {
-			return false, "Drone cannot move outside of Min X Point"
+			return false, errors.New("Drone cannot move outside of Min X Point")
 		}
 	case 4:
 		if result := drone.CurrentPoint.X + steps; result <= drone.MaxPoint.Y {
 			drone.CurrentPoint.X = result
-			return true, ""
+			return true, nil
 		} else {
-			return false, "Drone cannot move outside of Max X Point"
+			return false, errors.New("Drone cannot move outside of Max X Point")
 		}
 	default:
-		return false, "Undefined drone direction"
+		return false, errors.New("Undefined drone direction")
 	}
 }
 
 /*
 Generate available drone directions
 Output: map[uint]string
- */
+*/
 func DroneFace() map[uint]string {
 	return map[uint]string{
 		1: "UP",
